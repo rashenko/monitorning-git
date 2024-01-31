fc718bed3f9a87d8e4e07c19c62ed6259d0ae54e
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-31 11:26:30 +0300

    monitorning.sh

--- monitorning.sh
+++ monitorning.sh
@@ -16,12 +16,14 @@ cp /home/qqee/monitorning-git/prometheus.service /etc/systemd/system/prometheus.
 # Копируем конфиг prometheus
 cp /home/qqee/monitorning-git/prometheus.yml /etc/prometheus/prometheus.yml
 
+#Добавляем пользователей
+useradd --no-create-home --shell /usr/sbin/nologin prometheus
+
 # Создаём папки и копируем файлы prometheus
 mkdir {/etc/,/var/lib/}prometheus
 cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin
 cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/
 chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/
-
 
 # Перезапуск
 systemctl daemon-reload
