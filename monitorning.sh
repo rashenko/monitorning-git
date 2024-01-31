4e512be6f81fcb09d16ab0b0eb385c3d93dfa388
rashenko <149801050+rashenko@users.noreply.github.com>
2024-01-31 11:31:43 +0300

    monitorning.sh

--- monitorning.sh
+++ monitorning.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Добавляем пользователей node exporter
-useradd --no-create-home --shell /bin/false node_exporter
-
-# Копируем файлы node exporter в /usr/local
-cp node_exporter-*.linux-amd64/node_exporter /usr/local/bin
-chown node_exporter: /usr/local/bin/node_exporter
-
-# Копируем конфиг службы node exporter
-cp /home/qqee/monitorning-git/node_exporter.service /etc/systemd/system/node_exporter.service
-
 # Копируем конфиг prometheus.service
 cp /home/qqee/monitorning-git/prometheus.service /etc/systemd/system/prometheus.service
 
@@ -28,29 +18,14 @@ chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib
 # Перезапуск
 systemctl daemon-reload
 
-# Запускаем службу node_exporter
-systemctl start node_exporter
-
-# Проверяем статус службы node_exporter
-systemctl status node_exporter
-
-# Включаем автозапуск службы, при включении машины
-systemctl enable node_exporter
-
 # Запускаем сервис Prometheus
 systemctl start prometheus
-
-# Проверяем статус сервиса Prometheus
-systemctl status prometheus
 
 # Включаем автозапуск сервиса Prometheus
 systemctl enable prometheus
 
 # Запуск Grafana
 systemctl start grafana-server
-
-# Проверяем статус Grafana
-systemctl status grafana-server
 
 
 
