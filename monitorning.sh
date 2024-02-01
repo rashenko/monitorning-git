7a1f04952d7fcb01d3ff8218cbfe925a3a2f474c
rashenko <149801050+rashenko@users.noreply.github.com>
2024-02-01 09:01:58 +0300

    monitorning.sh

--- monitorning.sh
+++ monitorning.sh
@@ -11,8 +11,11 @@ useradd --no-create-home --shell /usr/sbin/nologin prometheus
 
 # Создаём папки и копируем файлы prometheus
 mkdir {/etc/,/var/lib/}prometheus
+
 cp -vi prometheus-*.linux-amd64/prom{etheus,tool} /usr/local/bin
+
 cp -rvi prometheus-*.linux-amd64/{console{_libraries,s},prometheus.yml} /etc/prometheus/
+
 chown -Rv prometheus: /usr/local/bin/prom{etheus,tool} /etc/prometheus/ /var/lib/prometheus/
 
 # Перезапуск
